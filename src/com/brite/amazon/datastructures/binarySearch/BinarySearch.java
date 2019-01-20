b052c1b296574f14da37dbb9ce68fe9b24ead50c
mveluru <mveluru@yahoo.com>
2019-01-19 20:21:54 -0600

    Muralidhar R Veluru

--- src/com/brite/amazon/datastructures/binarySearch/BinarySearch.java
+++ src/com/brite/amazon/datastructures/binarySearch/BinarySearch.java
@@ -2,35 +2,7 @@ package com.brite.amazon.datastructures.binarySearch;
 
 public class BinarySearch {
 	
-	//Sort the array and do binary search
-	/*Procedure binary_search
-	   A ← sorted array
-	   n ← size of array
-	   x ← value to be searched
 	
-	   Set lowerBound = 1
-	   Set upperBound = n 
-	
-	   while x not found
-	      if upperBound < lowerBound 
-	         EXIT: x does not exists.
-	   
-	      set midPoint = lowerBound + ( upperBound - lowerBound ) / 2
-	      
-	      if A[midPoint] < x
-	         set lowerBound = midPoint + 1
-	         
-	      if A[midPoint] > x
-	         set upperBound = midPoint - 1 
-	
-	      if A[midPoint] = x 
-	         EXIT: x found at location midPoint
-	   end while
-	   
-	end procedure
-	
-	 * 
-	 */
 	public int[] bubbleSort(int[] array) {
 		int len = array.length;
 		for (int m = len - 1; m >= 0; m--) {
